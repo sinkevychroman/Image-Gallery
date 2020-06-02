337fb52b11c99d8bdef6e74d59a6b05497cf4992
Roman Sinkevych <roman.sinkevych@hneu.net>
2020-06-02 14:09:06 +0300

    Configure workspace

---
+++ Image Gallery/ImageGalleryCollectionViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ImageGalleryCollectionViewController.swift
+//  Image Gallery
+//
+//  Created by Roman Sinkevych on 02.06.2020.
+//  Copyright © 2020 Roman Sinkevych. All rights reserved.
+//
+
+import UIKit
+
+
+class ImageGalleryCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Image Cell", for: indexPath)
+    
+        // Configure the cell
+    
+        return cell
+    }
+}
