75e582f954100179c76517e0a981707ef416c003
Roman Sinkevych <roman.sinkevych@hneu.net>
2020-06-02 13:51:26 +0300

    Initial Commit

---
+++ Image Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Gallery
+//
+//  Created by Roman Sinkevych on 02.06.2020.
+//  Copyright © 2020 Roman Sinkevych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
